099cfffa4f8ef0fccad92a9c30a62a5b228cf825
jessxleee <S10222321@connect.np.edu.sg>
2022-02-15 00:35:56 +0800

    login and signup form updated

--- script.js
+++ script.js
@@ -1,21 +1,7 @@
 
-$(document).ready(function(){
-  $("#myBtn").click(function(){
-    $("#myModal").modal();
-  });
-});
-
-$(document).ready(function(){
-      $("#gamesModal").modal('show');
-  });
-const switchers = [...document.querySelectorAll('.switcher')]
-
-switchers.forEach(item => {
-item.addEventListener('click', function() {
-  switchers.forEach(item => item.parentElement.classList.remove('is-active'))
-  this.parentElement.classList.add('is-active')
-})
-})
+function loginFunction(){
+  window.open("login.html");
+  }
 
 function function_crossword(){
 window.open("crossword.html");
