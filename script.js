b8242b18f692b246825ccc28feb94461b20d3c38
jessxleee <S10222321@connect.np.edu.sg>
2022-02-15 05:28:52 +0800

    added contents to dicover_2 page, index.page

--- script.js
+++ script.js
@@ -10,5 +10,23 @@ window.open("crossword.html");
 function function_hangman(){
 window.open("hangman.html");
 }
+function er1btnFunction(){
+  window.open("https://www.science.edu.sg/for-schools/resources");
+  }
+function er2btnFunction(){
+  window.open("https://www.science.edu.sg/for-schools/resources");
+  }
+function er3btnFunction(){
+  window.open("https://www.teachstarter.com/au/learning-area/science/");
+  }
+
+function changeImage() {
+  var image = document.getElementById('myImage');
+  if (image.src.match("images/dilationofeye2")) {
+    image.src = "images/dilationofeye1.jpg";
+  } else {
+    image.src = "images/dilationofeye2.jpg";
+  }
+}
 
 
