9d485b1eb0530d84d5f6b9589f2d2555a84b551b
harshini53 <s10204001@connect.np.edu.sg>
2022-02-17 03:27:47 +0800

    Added timer

--- script.js
+++ script.js
@@ -33,4 +33,30 @@ $(document).ready(function() {
   });
 });
 
+//Timer
+var timeInSecs;
+var ticker;
 
+function startTimer(secs) {
+  timeInSecs = parseInt(secs);
+  ticker = setInterval("tick()", 1000); 
+}
+
+function tick( ) {
+  var secs = timeInSecs;
+  if (secs > 0) {
+  timeInSecs--; 
+}
+else {
+  clearInterval(ticker);
+  startTimer(2*60); // 2 minutes in seconds
+}
+
+var mins = Math.floor(secs/60);
+secs %= 60;
+var pretty = ( (mins < 10) ? "0" : "" ) + mins + ":" + ( (secs < 10) ? "0" : "" ) + secs;
+
+document.getElementById("countdown").innerHTML = pretty;
+}
+
+startTimer(2*60); // 2 minutes in seconds
